6c590c40a33a4bf847148c5d22bfc68b05ec1aa1
Adam Schmideg <adam@schmideg.net>
2019-11-22 17:54:24 +0100

    Minimal web service

--- faucet.go
+++ faucet.go
@@ -2,13 +2,16 @@ package main
 
 import (
 	"fmt"
+	//"html/template"
+	"log"
+	"net/http"
 
 	"github.com/ethereum/go-ethereum/rpc"
 )
 
 const NodeID string = "fa25f829719067d8685332df3465bb877c2f49ee09587ae4afaa22623aaf3d7a"
 
-func main() {
+func connectToLightServer() {
 	client, _ := rpc.Dial("http://localhost:8545")
 
 	type ServerInfo struct {
@@ -59,4 +62,27 @@ func main() {
 		fmt.Println("Response", arrayResponse)
 	}
 	*/
+}
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	/*
+	t, err := template.ParseFiles("test.html")
+	if err != nil {
+		log.Println("Problem", err)
+	}
+	fillData := struct{}{}
+	*/
+
+	if r.Method == "POST" {
+		fmt.Fprintf(w, "addBalance(\"%s\"", r.FormValue("nodeID"))
+	} else {
+		//t.Execute(w, fillData)
+		fmt.Fprintf(w, "<h1>Hello</h1>")
+	}
+}
+
+func main() {
+	http.HandleFunc("/", rootHandler)
+    log.Fatal(http.ListenAndServe(":8080", nil))
 }
