3ad58b478a5f763cbbe017d474bcc2d0ad900dcc
Adam Schmideg <adam@schmideg.net>
2019-11-22 17:12:18 +0100

    Test some more rpc calls, some hang, though

--- faucet.go
+++ faucet.go
@@ -6,6 +6,8 @@ import (
 	"github.com/ethereum/go-ethereum/rpc"
 )
 
+const NodeID string = "fa25f829719067d8685332df3465bb877c2f49ee09587ae4afaa22623aaf3d7a"
+
 func main() {
 	client, _ := rpc.Dial("http://localhost:8545")
 
@@ -17,12 +19,44 @@ func main() {
 		TotalCapacity int
 		TotalConnectedCapacity int
 	}
-	var resp ServerInfo
-	//var resp map[string]interface{}
 
-	if err := client.Call(&resp, "les_serverInfo"); err != nil {
+	if false {
+		var dictResponse map[string]interface{}
+
+		if err := client.Call(&dictResponse, "les_serverInfo"); err != nil {
+			fmt.Println("Problem", err)
+		}	else {
+			fmt.Println("Response", dictResponse)
+		}
+
+		var arrayResponse []string
+
+		if err := client.Call(&arrayResponse, "les_getCheckpoint", 1); err != nil {
+			fmt.Println("Problem", err)
+		}	else {
+			fmt.Println("Response", arrayResponse)
+		}
+	}
+
+
+	/* 
+	// It hangs
+	var dictResponse map[string]interface{}
+
+	clientIDs := []string{NodeID}
+
+	if err := client.Call(&dictResponse, "les_clientInfo", clientIDs); err != nil {
 		fmt.Println("Problem", err)
 	}	else {
-		fmt.Println("Response", resp)
+		fmt.Println("Response", dictResponse)
 	}
+
+	var arrayResponse []string
+
+	if err := client.Call(&arrayResponse, "les_addBalance", NodeID, 10000000, "qwerty"); err != nil {
+		fmt.Println("Problem", err)
+	}	else {
+		fmt.Println("Response", arrayResponse)
+	}
+	*/
 }
