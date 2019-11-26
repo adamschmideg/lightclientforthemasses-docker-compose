67128a823514b5c06f84889fb174a1cc9920a38a
Adam Schmideg <adam@schmideg.net>
2019-11-26 12:13:24 +0100

    Clean up addBalance

--- balance.go
+++ balance.go
@@ -6,34 +6,43 @@ import (
 	"github.com/ethereum/go-ethereum/rpc"
 )
 
-func check(maybeError error) {
-	if maybeError != nil {
-		fmt.Println(maybeError)
-	}
-}
-func main() {
-	server, _ := rpc.Dial("http://127.0.0.1:8545")
-	client, _ := rpc.Dial("http://127.0.0.1:8546")
+func addBalance()(error) {
 	type NodeInfo struct {
 		ID string
 	}
-	var nodeInfo NodeInfo
-	client.Call(&nodeInfo, "admin_nodeInfo")
-	nodeID := nodeInfo.ID
-	fmt.Println("nodeID", nodeID)
 
-	var balance map[string]interface{}
-	if err := server.Call(&balance, "les_addBalance", nodeID, 100000, "foobar"); err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Println(balance)
+	server, err := rpc.Dial("http://127.0.0.1:8545")
+	if err != nil {
+		return fmt.Errorf("Server", err)
+	}
+	client, err := rpc.Dial("http://127.0.0.1:8546")
+	if err != nil {
+		return fmt.Errorf("Client", err)
 	}
 
-	var clientInfo map[string]interface{}
-	clientIDs := []string{nodeID}	
+	var nodeInfo NodeInfo
+	if err := client.Call(&nodeInfo, "admin_nodeInfo"); err != nil {
+		return fmt.Errorf("NodeInfo", err)
+	}
+
+	var balance []interface{}
+	if err := server.Call(&balance, "les_addBalance", nodeInfo.ID, 100000, "foobar"); err != nil {
+		return fmt.Errorf("balance", err)
+	}
+	fmt.Println(balance)
+
+	var clientInfo []interface{}
+	clientIDs := []string{nodeInfo.ID}	
 	if err := server.Call(&clientInfo, "les_clientInfo", clientIDs); err != nil {
+		return err
+	}
+	fmt.Println(clientInfo)
+	return nil
+}
+
+func main() {
+	err := addBalance()
+	if err != nil {
 		fmt.Println(err)
-	} else {
-		fmt.Println(clientInfo)
 	}
 }
