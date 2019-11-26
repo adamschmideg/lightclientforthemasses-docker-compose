d9fe2799b368bd4d0641e5a4facb03a68c4ce974
Adam Schmideg <adam@schmideg.net>
2019-11-26 16:02:14 +0100

    Skeleton of the web service

--- faucet.go
+++ faucet.go
@@ -9,62 +9,68 @@ import (
 	"github.com/ethereum/go-ethereum/rpc"
 )
 
-const NodeID string = "fa25f829719067d8685332df3465bb877c2f49ee09587ae4afaa22623aaf3d7a"
-
-func connectToLightServer() {
-	client, _ := rpc.Dial("http://localhost:8545")
-
-	type ServerInfo struct {
-		FreeClientCapacity int
-		MaximumCapacity int
-		MinimumCapacity int
-		PriorityConnectedCapacity int
-		TotalCapacity int
-		TotalConnectedCapacity int
-	}
-
-	if false {
-		var dictResponse map[string]interface{}
-
-		if err := client.Call(&dictResponse, "les_serverInfo"); err != nil {
-			fmt.Println("Problem", err)
-		}	else {
-			fmt.Println("Response", dictResponse)
-		}
-
-		var arrayResponse []string
-
-		if err := client.Call(&arrayResponse, "les_getCheckpoint", 1); err != nil {
-			fmt.Println("Problem", err)
-		}	else {
-			fmt.Println("Response", arrayResponse)
-		}
-	}
-
-
-	/* 
-	// It hangs
-	var dictResponse map[string]interface{}
-
-	clientIDs := []string{NodeID}
-
-	if err := client.Call(&dictResponse, "les_clientInfo", clientIDs); err != nil {
-		fmt.Println("Problem", err)
-	}	else {
-		fmt.Println("Response", dictResponse)
-	}
-
-	var arrayResponse []string
-
-	if err := client.Call(&arrayResponse, "les_addBalance", NodeID, 10000000, "qwerty"); err != nil {
-		fmt.Println("Problem", err)
-	}	else {
-		fmt.Println("Response", arrayResponse)
-	}
-	*/
+type BalanceInfo struct {
+	BalanceBefore int
+	BalanceAfter int
+	IsConnected bool
 }
 
+func AddBalance(serverRpcEndpoint string, clientNodeID string, balance int, topic string) (BalanceInfo,error) {
+	var info BalanceInfo
+	server, err := rpc.Dial(serverRpcEndpoint)
+	if err != nil {
+		return info, fmt.Errorf("Server", err)
+	}
+
+	var balances []int
+	if err := server.Call(&balances, "les_addBalance", clientNodeID, balance, topic); err != nil {
+		return info, fmt.Errorf("balance", err)
+	} 
+	info.BalanceBefore = balances[0]
+	info.BalanceAfter = balances[1]
+
+	return info, nil
+}
+
+func GetBalance(serverRpcEndpoint string, nodeID string) (BalanceInfo,error) {
+	var info BalanceInfo
+	server, err := rpc.Dial(serverRpcEndpoint)
+	if err != nil {
+		return info, fmt.Errorf("Server", err)
+	}
+
+	clientIDs := []string{nodeID}	
+	if err := server.Call(&info, "les_clientInfo", clientIDs); err != nil {
+		return info, fmt.Errorf("clientinfo", err)
+	}
+	return info, nil
+}
+
+const rpcEndpoint string = "http://127.0.0.1:8545"
+
 func rootHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	nodeID := r.FormValue("nodeID")
+	if nodeID == "" {
+		fmt.Fprint(w, "nodeID is required")
+	} else if r.Method == "POST" {
+		info, err := AddBalance(rpcEndpoint, nodeID, 1000, "foobar")
+		if err != nil {
+			fmt.Fprintf(w, "Can't add balance %s", err)
+		} else {
+			fmt.Fprintf(w, "Added balance %s", info)
+		}
+	} else if r.Method == "GET" {
+		info, err := GetBalance(rpcEndpoint, nodeID)
+		if err != nil {
+			fmt.Fprintf(w, "Can't get info %s", err)
+		} else {
+			fmt.Fprintf(w, "info %s", info)
+		}
+	}
+}
+
+func _rootHandler(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
 	/*
 	t, err := template.ParseFiles("test.html")
@@ -74,7 +80,16 @@ func rootHandler(w http.ResponseWriter, r *http.Request) {
 	fillData := struct{}{}
 	*/
 
+	nodeID := r.FormValue("nodeID")
+	if nodeID == "" {
+		fmt.Fprint(w, "nodeID is required")
+	} else if r.Method == "POST" && r.FormValue("cmd") == "addBalance" {
+	}
 	if r.Method == "POST" {
+		cmd := r.FormValue("cmd")
+		if cmd == "addBalance" {
+
+		}
 		fmt.Fprintf(w, "addBalance(\"%s\"", r.FormValue("nodeID"))
 	} else {
 		//t.Execute(w, fillData)
@@ -84,5 +99,5 @@ func rootHandler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/", rootHandler)
-    log.Fatal(http.ListenAndServe(":8080", nil))
+    log.Fatal(http.ListenAndServe(":8088", nil))
 }
