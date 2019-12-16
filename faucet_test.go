d66ee481dff09d7c142a84cce2df98626d90748e
Adam Schmideg <adam@schmideg.net>
2019-12-16 16:29:15 +0100

    Automatic testing with `make test`

--- faucet_test.go
+++ faucet_test.go
@@ -9,11 +9,21 @@ import (
     "testing"
 )
 
-func TestThrottling(t *testing.T) {
-	wsaddr := flag.String("wsaddr", "127.0.0.1", "Address of the faucet")
+
+var wsaddr = flag.String("wsaddr", "127.0.0.1", "Address of the faucet")
+func Init() {
 	flag.Parse()
-	ip, _ := net.LookupIP(*wsaddr)
+}
+
+func TestThrottling(t *testing.T) {
+	ips, err := net.LookupIP(*wsaddr)
+	if len(ips) == 0 || err != nil {
+		t.Error("Cannot resolve", *wsaddr)
+		return
+	}
+	ip := ips[0].String()
 	endpoint := fmt.Sprintf("http://%s:%v", ip, "8088")
+	fmt.Println("debug", ip, endpoint)
 	
 	resp1, err := http.Get(endpoint)
 	if err != nil {
