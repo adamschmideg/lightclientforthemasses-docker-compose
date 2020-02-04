b130830890e8d45f06a3c223fb4f99cc02e41546
Adam Schmideg <adam@schmideg.net>
2020-02-04 12:41:49 +0100

    Get enode in demo

--- demo_test.go
+++ demo_test.go
@@ -3,24 +3,54 @@ package main
 import (
 	"os/exec"
 	"testing"
+	"time"
 )
 
-func TestDemo(t *testing.T) {
-	commonArgs := []string{"--rpc", "--rpcap=admin,eth,les", "--rpcaddr=0.0.0.0"}
-// Start lightserver with 1 slot for light clients
+func TestEnode(t *testing.T) {
+	var err error
 	serverDatadir := "~/datadirs/goerli/fast"
+	// Get the server's enode
+	enodeExec := exec.Command("geth", "--datadir", serverDatadir, "attach", "--exec", "admin.nodeInfo.enode" )
+	var out []byte
+	out, err = enodeExec.CombinedOutput()
+	if err != nil {
+		t.Error("enode", err)
+	}
+	enode := string(out)
+	t.Log("yes", enode)
+}
+
+func TestDemo(t *testing.T) {
+	var err error
+	commonArgs := []string{"--rpc", "--rpcap=admin,eth,les", "--rpcaddr=0.0.0.0"}
+	// Start lightserver with 1 slot for light clients
+	serverDatadir := "./datadirs/goerli/fast"
 	serverArgs :=  []string{"--lightserv=100", "--light.maxpeers=1", "--datadir", serverDatadir, "--goerli"}
 	var args []string
 	args = append([]string{}, commonArgs...)
 	args = append(args, serverArgs...)
 	cmdServer := exec.Command("geth", serverArgs...)
-	if err := cmdServer.Start(); err != nil {
+	if err = cmdServer.Start(); err != nil {
 		t.Error("start", err)
 	}
-	if err := cmdServer.Process.Kill(); err != nil {
+
+	// Get the server's enode
+	time.Sleep(1 * time.Second) // wait before we can attach to it
+	enodeExec := exec.Command("geth", "attach", "--exec", "admin.nodeInfo.enode", "./datadirs/goerli/fast/geth.ipc")
+	var out []byte
+	out, err = enodeExec.CombinedOutput()
+	if err != nil {
+		t.Error("enode", err, string(out))
+		out = nil
+	}
+	enode := string(out)
+	t.Log("yes", enode)
+
+	// Tear down: kill all process
+	if err = cmdServer.Process.Kill(); err != nil {
 		t.Error("kill", err)
 	}
-// Get the server's enode
+
 // Start a light client with an empty datadir
 // Add the server as peer to let this client start syncing
 // Start a priority client with an empty datadir
